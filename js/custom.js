c1cd9cbee7b8758713efbb50773286a4a3f39441
asteroidice <ryan.s.rabello@gmail.com>
2016-09-12 11:42:21 -0700

    Added mask search

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     speed:"auto",
     autoCoefficient:2
   });
+  $('#maskSearchQuery').keypress(function (e) {
+    if (e.which == 13) {
+      if(e.currentTarget.value){
+        maskSearch();
+        return false;
+      }
+    }
+  });
 });
+
+function maskSearch()  {
+  var query = $("#maskSearchQuery").val();
+  window.location = "https:aswwu.com/#/search/" + query;
+}
