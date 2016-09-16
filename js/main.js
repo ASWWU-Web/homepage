6b2c66dee633a514eb4a58883d2d1be0bb9219b3
asteroidice <ryan.s.rabello@gmail.com>
2016-09-16 14:29:52 -0700

    CORS sucks

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
     speed:"auto",
     autoCoefficient:2
   });
+  var url = "https://search.issuu.com/api/2_0/document?username=aswwucollegian&pageSize=10&responseParams=title,description&sortBy=epoch";
+  $.getJSON(url, function(data) {
+    console.log(data);
+  });
+  // var settings = {
+  //   "url": url,
+  //   "jsonpCallback": function(data) {console.log("jsonpCallback()",data);},
+  //   "success": function(data) {console.log("success()",data);},
+  //   "crossDomain": true,
+  //   "headers": "Access-Control-Allow-Origin: *"
+  // };
+  // $.get(settings);
 });
