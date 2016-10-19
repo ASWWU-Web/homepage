d1a5204c939aa46a931397a723a0e4b49e3be10e
asteroidice <ryan.s.rabello@gmail.com>
2016-10-18 20:57:26 -0700

    changed collegian to logo

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
     speed:"auto",
     autoCoefficient:2
   });
-  $.getJSON("https://aswwu.com/server/feed?name=issuu", function(data) {
-    if(data.response.docs[0].docname)
-      $("#collegianContainer").prepend("<div class='embed-responsive embed-responsive-16by9'><div data-url='https://issuu.com/aswwucollegian/docs/" + data.response.docs[0].docname + "' class='issuuembed embed-responsive-item'></div><script type='text/javascript' src='//e.issuu.com/embed.js' async='true'></script></div>");
-    else
-      $("#collegianContainer").prepend("<p class='text-danger'>Couldn't get the latest Collegian. :(</p>");
-  }).fail(function() {
-    $("#collegianContainer").prepend("<p class='text-danger'>Couldn't get the latest Collegian. :(</p>");
-  });
+  // $.getJSON("https://aswwu.com/server/feed?name=issuu", function(data) {
+  //   if(data.response.docs[0].docname)
+  //     $("#collegianContainer").prepend("<div class='embed-responsive embed-responsive-16by9'><div data-url='https://issuu.com/aswwucollegian/docs/" + data.response.docs[0].docname + "' class='issuuembed embed-responsive-item'></div><script type='text/javascript' src='//e.issuu.com/embed.js' async='true'></script></div>");
+  //   else
+  //     $("#collegianContainer").prepend("<p class='text-danger'>Couldn't get the latest Collegian. :(</p>");
+  // }).fail(function() {
+  //   $("#collegianContainer").prepend("<p class='text-danger'>Couldn't get the latest Collegian. :(</p>");
+  // });
 });
--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+git acp 
 ssh aswwu.com git -C /var/www/html/aswwu.com/dev/ pull
