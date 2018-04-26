88c51f721ec17bb84cc2b6c46d11386f30dc26b6
sermshar <stephen.ermshar@wallawalla.edu>
2018-04-25 17:30:47 -0700

    gets all search results and displays them in typeahead sections

--- src/app/services/super-duper.service.ts
+++ src/app/services/super-duper.service.ts
@@ -5,18 +5,20 @@ import {of} from 'rxjs/observable/of';
 import {map} from 'rxjs/operators';
 
 import { RequestService } from './request.service';
-// import CURRENT_YEAR
+import { CURRENT_YEAR } from '../config';
+import { concat } from 'rxjs/operators/concat';
+import 'rxjs/add/observable/forkJoin';
 
 @Injectable()
 export class SuperDuperService {
 
-  jobsUri = '/server/forms/job/view/all';
-  maskUri = 'search/1718/';
+  jobsUri = '/forms/job/view/all';
+  maskUri = 'search/' + CURRENT_YEAR + '/';
   pagesUri = '/pages/search?general=';
 
   jobLink = 'https://aswwu.com/jobs/submit/';
-  maskLink = '';
-  pageLink = '';
+  maskLink = 'https://aswwu.com/mask/profile/';
+  pageLink = 'https://aswwu.com/pages/';
 
   constructor(private requests: RequestService) { }
 
@@ -26,69 +28,67 @@ export class SuperDuperService {
     return query;
   }
 
-  // jobsFilter(jobs) {
+  filterJobs(query, item) {
+    query = query.toLowerCase();
+    const visible = item.visibility;
+    const inDescription = false;//item.job_description.search(query) !== -1;
+    const inName = item.job_name.search(query) !== -1;
+    const show = visible && (inDescription || inName);
+    return show;
+  }
 
-  // }
+  mapMask( response ) {
+    let toReturn = response.results.slice(0, 3).map(result => {
+      return {
+        'main': result.full_name,
+        'sub': '', // we could add a sub text for each result, like major for mask results
+        'source': 'Mask',
+        'link': this.maskLink + result.username,
+        'top': false,
+      };
+    });
+    toReturn[0].top = true;
+    return toReturn;
+  }
 
-  // search(query: string) {
-  //   const queryUri = this.parseQuery(query);
-  //   let jobsResults: any;
-  //   let maskResults: any;
-  //   let pagesResults: any;
-  //   this.requests.get((this.jobsUri + queryUri), (data) => jobsResults = this.jobsFilter(data.forms), null);
-  //   this.requests.get((this.maskUri + queryUri), (data) => maskResults = data.results, null);
-  //   this.requests.get((this.pagesUri + queryUri), (data) => pagesResults = data, null);
-  //   return of({
-  //     'jobs': jobsResults,
-  //     'mask': maskResults,
-  //     'pages': pagesResults
-  //   });
-  // }
+  mapJobs( query, response ) {
+    let toReturn = response.forms.filter(item => this.filterJobs(query, item)).slice(0, 3).map(result => {
+      return {
+        'main': result.job_name,
+        'sub': '',
+        'source': 'Jobs',
+        'link': this.jobLink + result.jobID,
+        'top': false,
+      };
+    });
+    toReturn[0].top = true;
+    return toReturn;
+  }
 
-  // searchArray(query: string) {
-  //   let rl = of([]);
-  //   if (query === '') {
-  //     return of([]);
-  //   } else {
-  //     this.search(query).do( (data) => {
-  //       const jobsMap = data.jobs.map( (job) => { return {'result': job.job_name,
-  //                                                         'sub': 'ASWWU Jobs',
-  //                                                         'link': this.jobLink + job.jobID}; });
-  //       const maskMap = data.mask.map( (profile) => { return {'result': profile.full_name,
-  //                                                             'sub': 'Mask',
-  //                                                             'link': this.maskLink}; });
-  //       const pagesMap = data.mask.map( (page) => { return {'result': page.,
-  //                                                              'sub': 'Mask',
-  //                                                              'link': this.maskLink}; });
-  //         rl = of(
-  //         {
-  //           'jobs': jobsMap,
-  //           'mask': maskMap,
-  //           'pages': pagesMap
-  //         });
-  //     });
-  //     return resultList;
-  //   }
-  //   // WIP: this takes an observable from search and maps the results to an observable array which will later be handled by the typeahead
-  //   // const resultsObservable = this.search(query);
-  //   // const results.do( (info) => {
-  //   //   let resultsArray = 0;
-  // }
+  mapPages( response ) {
+    let toReturn = response.results.slice(0, 3).map(result => {
+      return {
+        'main': result.title,
+        'sub': '',
+        'source': 'Pages',
+        'link': this.pageLink + result.url,
+        'top': false,
+      };
+    });
+    toReturn[0].top = true;
+    return toReturn;
+  }
 
   SearchAndReturnObservableArray(query) {
     const queryUri = this.parseQuery(query);
-    return this.requests.getObservable( this.maskUri + queryUri ).map(response => {
-      let toReturn = response.results.map(result => {
-        return {
-          'main': result.full_name,
-          'source': 'Mask',
-          'link': 'somefakeurlfortesting',
-          'top': false,
-        };
-      });
-      toReturn[0].top = true;
-      return toReturn;
+    let maskObservable = this.requests.getObservable( this.maskUri + queryUri ).map(response => this.mapMask(response)).catch(err => of([]));
+    let pagesObservable = this.requests.getObservable( this.pagesUri ).map(response => this.mapPages(response)).catch(err => of([]));
+    let jobsObservable = this.requests.getObservable( this.jobsUri ).map(response => this.mapJobs(query, response)).catch(err => of([])); // don't parseQuery()
+    // uses: https://stackoverflow.com/questions/44141569/how-to-concat-two-observable-arrays-into-a-single-array
+    let toReturn = Observable.forkJoin(maskObservable, pagesObservable, jobsObservable).map(([s1, s2, s3]) => {
+      return [...s1, ...s2, ...s3];
     });
+    return toReturn;
   }
 
 
