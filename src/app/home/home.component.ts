1e49ac91edc02c62465a20e678e769009111e710
sermshar <stephen.ermshar@wallawalla.edu>
2018-04-30 17:29:34 -0700

    WIP: background image fading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,23 @@ import { NgStyle } from '@angular/common';
 export class HomeComponent implements OnInit {
 
   bgLink = 'https://aswwu.com/media/background.php';
+  bgLinkChanged = 'url(' + this.bgLink + ')';
+  // bgLinkChanged = this.bgLink;
 
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+      this.generateNewBgLink();
+    }, 10000);
+  }
 
+  private generateNewBgLink() {
+    const d = new Date();
+    const time = d.getTime();
+    this.bgLinkChanged = 'url(../../assets/blank.png)';
+    setTimeout(() => {
+      this.bgLinkChanged = 'url(' + this.bgLink + '?' + time + ')';
+    }, 2000);
   }
 }
