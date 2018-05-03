f668964832fa1563a330835995384bd01cdaae93
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-05-02 17:48:36 -0700

    Fixed missing profile bug. Added margin to searching...

--- src/app/resolveMaskImage.ts
+++ src/app/resolveMaskImage.ts
@@ -2,7 +2,7 @@ import { environment } from '../environments/environment';
 import { MEDIA_URI, MEDIA_SM } from './config';
 
 export function resolveMaskImage(photo_URI:string, media_link:string = MEDIA_URI) {
-  if (photo_URI) {
+  if (photo_URI && photo_URI != 'None') {
     return (media_link + '/' + photo_URI);
   } else {
     return '';
