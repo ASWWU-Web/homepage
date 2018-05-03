75dac7153f1ea218d644324aba1ed9e2c64ae915
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-05-02 23:07:24 -0700

    Merged develop into feature/super-duper.

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, NgModule, OnInit} from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: '<div><upcoming-events></upcoming-events></div>'
-})
-
-export class AppComponent { }
