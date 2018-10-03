d7b93f1771bcf4dae5977e229f9ba049ae139eb7
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-10-03 15:46:00 -0700

    add todo list

--- src/app/senate-election/senate-election.component.ts
+++ src/app/senate-election/senate-election.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SenateElectionComponent implements OnInit {
 
+  // TODO:
+  //    on initialization get list of candidates (alternatively wait until "Go" and have server only return candidates for one district)
+  //    Show the first page with district
+  //    bind variable to district choice
+  //    Go button that sets show candidates to true
+  //    Set districts html to show only when show-candidates is false
+  //    Set candidates html to show only when show-candidates is true and candidate list exists
+  //    on "Go" build district-candidate list based on selected district
+  //      fetch display names and images for district candidate list if we decide not to included 
+  //        them in original candidate list
+  //    use ngfor on district-candidate list
+  //    use ngstyle to set unselected pictures to faded state when number of selected pictures + input boxes fillled >= 2
+  //    set empty write-in inputs to inactive (grayed out) state when number of selected pictures + input boxes fillled >=2
+  //
+
   constructor() { }
 
   ngOnInit() {
