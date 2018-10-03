0d6b32278760e6deb39eb38b0b4562561c9504fc
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-10-03 09:00:00 -0700

    add senate-election component

---
+++ src/app/senate-election/senate-election.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-senate-election',
+  templateUrl: './senate-election.component.html',
+  styleUrls: ['./senate-election.component.css']
+})
+export class SenateElectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
