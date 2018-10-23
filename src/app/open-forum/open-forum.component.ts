ad2c2bdf8562af77d197e552abcd208dd79e05ee
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-10-23 14:56:15 -0700

    add open forum component

---
+++ src/app/open-forum/open-forum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'open-forum',
+  templateUrl: './open-forum.component.html',
+  styleUrls: ['./open-forum.component.css']
+})
+export class OpenForumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
