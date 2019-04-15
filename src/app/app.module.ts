46a2da2c0f16fdbd1668bdafdb907243fb6c757c
morini <nicholas.morin@wallawalla.edu>
2019-04-15 16:10:13 -0700

    updated app component and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RequestService, SuperDuperService } from './services/services';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { UpcomingComponent } from './upcoming/upcoming.component';
+import { CalendarService } from './services/calendar.service';
+import { UserBubbleComponent } from './user-bubble/user-bubble.component';
+import { HomeComponent } from './home/home.component';
+import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
+import { SuperDuperComponent } from './super-duper/super-duper.component';
+
+import { SearchComponent } from './search/search.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { PageCardComponent } from './page-card/page-card.component';
+import { JobCardComponent } from './job-card/job-card.component';
+import { ScrollCardsComponent } from './scroll-cards/scroll-cards.component';
+import { OpenForumComponent } from './open-forum/open-forum.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MobileNavComponent,
+    FooterComponent,
+    UpcomingComponent,
+    UserBubbleComponent,
+    HomeComponent,
+    SuperDuperComponent,
+    SearchComponent,
+    ProfileCardComponent,
+    PageCardComponent,
+    JobCardComponent,
+    ScrollCardsComponent,
+    OpenForumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
   ],
-  providers: [],
+  providers: [RequestService, CalendarService, SuperDuperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
